6c9981c20b4f1210c9ef4b7c08d25ab200afdc9d
newlls <523786175@qq.com>
2019-04-28 17:30:06 +0800

    这里是我的一些小练习啦

---
+++ vue-cli/app/src/components/Footer.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container" :style="{background:color}">
+        <ul class="content">
+            <li v-for="(obj,index) in menu" :key="index">
+                <router-link :to="obj.path" @click.native="change(obj)">{{obj.name}}</router-link>
+            </li>
+
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:["menu","color"],
+        data(){
+            return {
+
+            }
+        },
+        methods:{
+            change(obj){
+                this.$emit('changeBg',obj)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .container{
+        height: 1rem;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        background: #ff0000;
+        width: 100%;
+        line-height: 1rem;
+
+    }
+    .content{
+        display: flex;
+    }
+    .content li{
+        flex-grow: 1;
+        text-align: center;
+        font-size: .3rem;
+    }
+    .content a{
+        outline: none;
+        color: #555;
+    }
+    .content a.router-link-active{
+        color: #fff;
+    }
+</style>
