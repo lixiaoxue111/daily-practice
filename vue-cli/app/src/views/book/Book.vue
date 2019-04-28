761df832735cb597bc1172d6881d3ee25fa071f3
newlls <523786175@qq.com>
2019-04-28 19:55:40 +0800

    日常练习啦~~

---
+++ vue-cli/app/src/views/book/Book.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <slide :imgs="imgs"></slide>
+    </div>
+</template>
+
+<script>
+    import Slide from '@/components/Slider.vue'
+    export default {
+        components:{
+            Slide
+        },
+        data(){
+            return{
+                imgs:[
+                    "https://img3.doubanio.com/lpic/s24468373.jpg",
+                    "https://img3.doubanio.com/lpic/s27102925.jpg",
+                    "https://img3.doubanio.com/lpic/s6989253.jpg"
+                ],
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
