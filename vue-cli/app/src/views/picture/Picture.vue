5c909f8ffa739db72679f0da7c1f63e55550eeba
newlls <523786175@qq.com>
2019-04-28 19:57:31 +0800

    日常练习啦~~

---
+++ vue-cli/app/src/views/picture/Picture.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <img v-for="(imgSrc,index) in $store.state.photos" :key="index"
+            :src="imgSrc.src" @click="goDetail(index)"
+        />
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        created(){
+            axios.get('/data/photodata.json').then((result)=>{
+                console.log(result);
+                this.$store.commit('addPhoto',result.data.photoData)
+            }).catch((err)=>{
+                console.log(err);
+            })
+        },
+        methods:{
+            goDetail(index){
+                this.$router.push('/pictureDetail/'+index);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+img{
+    width: 50%;
+}
+</style>
